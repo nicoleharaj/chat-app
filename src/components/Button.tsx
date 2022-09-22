a0ceb3c88742f5412de81d481c2d280cdd68d10f
Nicole Haraj <nicoleharaj@outlook.com>
2022-09-22 00:08:30 -0400

    update Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,30 @@
-import classNames from 'classnames';
 import { ReactElement } from 'react';
+import classNames from 'classnames';
 
-type B = {
+export interface ButtonProps {
   size?: string;
-  type?: string;
+  primary?: boolean;
+  secondary?: boolean;
+  success?: boolean;
+  danger?: boolean;
+  warning?: boolean;
+  info?: boolean;
   className?: string;
   children?: ReactElement | string;
-};
+}
 
-export default function Button({ size, type, className, children }: B) {
+export default function Button({ size, primary, secondary, success, danger, warning, info, className, children }: ButtonProps) {
   return (
     <button
       className={classNames(
         'outline-none rounded-md font-medium active:scale-95 focus:ring-none disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed',
         {
-          'bg-indigo-600 text-white hover:bg-indigo-700 px-2 py-1': type === 'primary',
-          'bg-slate-600 text-white hover:bg-slate-700 px-2 py-1': type === 'secondary',
-          'bg-emerald-600 text-white hover:bg-emerald-700 px-2 py-1': type === 'success',
-          'bg-rose-600 text-white hover:bg-rose-700 px-2 py-1': type === 'danger',
-          'bg-yellow-300 text-neutral-700 hover:bg-yellow-400 px-2 py-1': type === 'warning',
-          'bg-sky-500 text-white hover:bg-sky-600 px-2 py-1': type === 'info',
+          'bg-indigo-600 text-white hover:bg-indigo-700 px-2 py-1': primary,
+          'bg-slate-600 text-white hover:bg-slate-700 px-2 py-1': secondary,
+          'bg-emerald-600 text-white hover:bg-emerald-700 px-2 py-1': success,
+          'bg-rose-600 text-white hover:bg-rose-700 px-2 py-1': danger,
+          'bg-yellow-300 text-neutral-700 hover:bg-yellow-400 px-2 py-1': warning,
+          'bg-sky-500 text-white hover:bg-sky-600 px-2 py-1': info,
         },
         {
           'text-xs': size === 'xs',
